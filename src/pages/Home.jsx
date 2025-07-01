62dd8381a016a120698a5072f951782b641559f6
aufardp <aufardanu@gmail.com>
2025-07-01 17:21:51 +0700

    feat: stars and meteors background

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,13 @@
+import { StarBackground } from "../components/StarBackground";
+import { ThemeToggle } from "../components/ThemeToggle";
+
 export const Home = () => {
   return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
-      <h1>Ini Home</h1>
       {/* Theme toogle */}
+      <ThemeToggle />
       {/* Background effect */}
+      <StarBackground />
       {/* Navbar */}
       {/* Main content */}
       {/* Footer */}
