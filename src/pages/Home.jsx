0b4e77ccf49a59c69ac513e5d0902124f5b5cc82
aufardp <aufardanu@gmail.com>
2025-07-01 15:04:00 +0700

    init project

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+export const Home = () => {
+  return (
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+      <h1>Ini Home</h1>
+      {/* Theme toogle */}
+      {/* Background effect */}
+      {/* Navbar */}
+      {/* Main content */}
+      {/* Footer */}
+    </div>
+  );
+};
